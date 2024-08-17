f4472d173914513a2d72c069a3e8bd0e05d9dd7f
Srujankm12 <srujankm12@gmail.com>
2024-08-17 23:03:56 +0530

    done bank-project

--- bankproject/bank.go
+++ bankproject/bank.go
@@ -1,66 +1,78 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
-
-func main(){
-	var accountBalance float64= 10000
-	
-	fmt.Println("Welcome to Go bank!")
-	
-	for {
-
-	fmt.Println("What do you want to do?")
-	fmt.Println("1. Check balance")
-	fmt.Println("2. Deposit money")
-	fmt.Println("3. withdraw money")
-	fmt.Println("4. Exit ")
-				
-	var choice int
-	fmt.Println("Your Choice: ")
-	fmt.Scan(&choice)
-	
-	//wantsCheckBalance :=choice == 1
-	
-	if  choice == 1{
-		fmt.Println("your balance is :",accountBalance)
-	}else if choice == 2 {
-		fmt.Println("Your deposit :")
-		var depositAmount float64
-		fmt.Scan(&depositAmount)
-		
-		if depositAmount <= 0{
-			fmt.Println("Invalid amount ,Must be greater than 0")
-		//	return
-		continue
-		}
-		accountBalance = accountBalance + depositAmount
-		fmt.Println("Balance updated! new Amount after deposit is :",accountBalance)	
-	}else if choice == 3{
-		fmt.Println("Withdraw money:")
-		var withdrawmoney float64
-		fmt.Scan(&withdrawmoney)
-		
-		if withdrawmoney <= 0{
-			fmt.Println("Invalid amount!")
-			break
-			//return
-		}
-		if withdrawmoney >accountBalance {
-			fmt.Println("You cant withdraw more than you have")
-			//return
-			break
-		}
-		
-		
-		accountBalance -=withdrawmoney
-		fmt.Println("Balance updated! new amount after withdrawing the money is :",accountBalance)	
-	} else{
-		fmt.Println("Goodbye")
-	//	return
-	break
+func writeBalanceToFile(balance float64) {
+	balanceText := fmt.Sprint(balance)
+	err := os.WriteFile("balance.txt", []byte(balanceText), 0644)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+	} else {
+		fmt.Println("Balance saved to file.")
 	}
-
 }
-fmt.Println("Thanks for choosing our Bank")
+
+func main() {
+	var accountBalance float64 = 10000
+
+	fmt.Println("Welcome to Go Bank Vsense!")
+
+	for {
+		fmt.Println("\nWhat do you want to do?")
+		fmt.Println("1. Check balance")
+		fmt.Println("2. Deposit money")
+		fmt.Println("3. Withdraw money")
+		fmt.Println("4. Exit ")
+
+		var choice int
+		fmt.Print("Your Choice: ")
+		fmt.Scan(&choice)
+
+		switch choice {
+		case 1:
+			fmt.Println("Your balance is:", accountBalance)
+		case 2:
+			fmt.Print("Enter deposit amount: ")
+			var depositAmount float64
+			fmt.Scan(&depositAmount)
+
+			if depositAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0.")
+				continue
+			}
+
+			accountBalance += depositAmount
+			fmt.Println("Balance updated! New balance after deposit is:", accountBalance)
+			writeBalanceToFile(accountBalance)
+
+		case 3:
+			fmt.Print("Enter withdrawal amount: ")
+			var withdrawAmount float64
+			fmt.Scan(&withdrawAmount)
+
+			if withdrawAmount <= 0 {
+				fmt.Println("Invalid amount!")
+				continue
+			}
+			if withdrawAmount > accountBalance {
+				fmt.Println("You can't withdraw more than you have.")
+				continue
+			}
+
+			accountBalance -= withdrawAmount
+			fmt.Println("Balance updated! New balance after withdrawal is:", accountBalance)
+			writeBalanceToFile(accountBalance)
+
+		case 4:
+			fmt.Println("Goodbye!")
+			fmt.Println("Thanks for choosing our bank.")
+			return
+
+		default:
+			fmt.Println("Invalid choice. Please try again.")
+		}
+	}
 }
